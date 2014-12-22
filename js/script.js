a24785ab84fdc56d4d67dd522b307092c76d0536
Randy Davis <randy_davis@outlook.com>
2014-12-22 13:18:36 -0600

    add random cell placement

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ var cells;
 $(function() {
   table = $('#main');
   initializeGame();
+  cells = table.find('td');
+  placeRandomCells();
 });
 
 function initializeGame() {
@@ -19,4 +21,22 @@ function initializeGame() {
   }
   trHtml = trHtml.join('');
   table.append($(trHtml));
+}
+
+function placeRandomCells() {
+  for (var y = 0; y < dimension; y++) {
+    for (var x = 0; x < dimension; x++) {
+      var cell = getCell(x, y);
+      if(Math.random() > chanceOfLiveCell) cell.addClass('alive');
+      else { cell.removeClass('alive');}
+    }
+  }
+}
+
+function getCell(x, y) {
+  if (x >= dimension) { x = 0; }
+  if (y >= dimension) { y = 0; }
+  if (x < 0) { x = dimension - 1; }
+  if (y < 0) { y = dimension - 1; }
+  return $(cells[y * dimension + x]);
 }
